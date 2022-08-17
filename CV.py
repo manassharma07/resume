b00d5b6d567a6875ef77f8f4e10a4d22c67f88fa
manas sharma <mikeatthetop@gmail.com>
2022-08-17 23:11:38 +0200

    many more changes

--- CV.py
+++ CV.py
@@ -23,11 +23,15 @@ with col2:
 
 with col3:
     st.write(' ')
-st.markdown('## Summary', unsafe_allow_html=True)
+st.write('## Research Interest', unsafe_allow_html=True)
 st.info('''
-- Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
-- Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
-- Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
+My passion lies in **developing codes and methods** to model the **light-matter interaction of hybrid systems**. More specifically, I am working on improving and accelerating **quantum embedding** techniques for molecular and periodic systems. 
+A core focus of my Ph.D. has been on implementing **density functional theory (DFT) based embedding techniques** and coupling them to real time-time dependent DFT (**RT-TDDFT**) and wavefunction theory (**WFT**) methods. 
+Most notably, I was able to calculate the excitation energies of solvated molecules and adsorption energies of **molecule-periodic** systems using **WFT-in-DFT** method at only a fraction of cost of a traditional calculation.
+More recently, my work has been focused on accurate molecule-in-periodic embedding and high harmonic generation via RT-TDDFT. 
+Recently, I have also started taking interest in **deep learning** and created a performant neural network library from scratch which supports parallelization and GPUs.
+I possess strong verbal, presentation and written communication skills as demonstrated by extensive **participation in `>10` conferences (`5` talks; `5` posters)** as well as publishing **`5` research articles**.
+Besides all this, I love to make YouTube tutorials, web & android apps, and computer software/libraries for researchers and students.
 ''')
 
 #####################
@@ -37,7 +41,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Chanin Nantasenamat</a>
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Manas Sharma</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -47,16 +51,16 @@ st.markdown("""
         <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#education">Education</a>
+        <a class="nav-link" href="#news">News</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#work-experience">Work Experience</a>
+        <a class="nav-link" href="#about">About</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#bioinformatics-tools">Bioinformatics Tools</a>
+        <a class="nav-link" href="#code">Code Dev</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#social-media">Social Media</a>
+        <a class="nav-link" href="#social-media">Contact</a>
       </li>
     </ul>
   </div>
@@ -96,93 +100,93 @@ def txt4(a, b, c):
     st.markdown(c)
 
 #####################
-st.markdown('''
-## Education
-''')
+# st.markdown('''
+# ## Education
+# ''')
 
-txt('**Doctor of Philosophy** (Medical Technology), *Mahidol University*, Thailand',
-'2002-2006')
-st.markdown('''
-- GPA: `3.89`
-- Research thesis entitled `Computer-aided molecular design for biological and chemical applications : Quantum chemical and machine learning approach`.
-- Received Royal Golden Jubilee Ph.D. Scholarship of `2.152 million THB` covering tuition and stipend.
-- Thesis awarded `1st` Prize by the National Research Council of Thailand.
-''')
+# txt('**Doctor of Philosophy** (Medical Technology), *Mahidol University*, Thailand',
+# '2002-2006')
+# st.markdown('''
+# - GPA: `3.89`
+# - Research thesis entitled `Computer-aided molecular design for biological and chemical applications : Quantum chemical and machine learning approach`.
+# - Received Royal Golden Jubilee Ph.D. Scholarship of `2.152 million THB` covering tuition and stipend.
+# - Thesis awarded `1st` Prize by the National Research Council of Thailand.
+# ''')
 
-txt('**Bachelors of Science** (Biological Science), *Mahidol University International College*, Thailand',
-'1998-2002')
-st.markdown('''
-- GPA: `3.65`
-- Graduated with First Class Honors.
-''')
+# txt('**Bachelors of Science** (Biological Science), *Mahidol University International College*, Thailand',
+# '1998-2002')
+# st.markdown('''
+# - GPA: `3.65`
+# - Graduated with First Class Honors.
+# ''')
+
+#####################
+# st.markdown('''
+# ## Work Experience
+# ''')
+
+# txt('**Head, Center of Data Mining and Biomedical Informatics**, Faculty of Medical Technology, Mahidol University, Thailand',
+# '2011-2021')
+# st.markdown('''
+# - Managing a Center of `10` professors, researchers and students to ensure KPIs are strategically achieved namely to publish at least `20+` research publications annually. 
+# - Actively took part in the talent hiring process as well as help employees to plan and develop their career path.
+# - Set budget and handle procurement in order to facilitate education and research activities. Secured `> 10 million THB` budget.
+# - Set and reflect on OKR on an annual basis to ensure productivity strategically matches the organization's direction.
+# ''')
+
+# txt('**Associate Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
+# '2012-2021')
+# txt('**Assistant Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
+# '2009-2012')
+# txt('**Lecturer**, Faculty of Medical Technology, Mahidol University, Thailand',
+# '2006-2009')
+# st.markdown('''
+# - Provided mentorship and supervision to junior faculty, researchers, Ph.D./M.Sc./B.Sc. students. Mentored `3` Post-doctoral fellows, supervised `13` Ph.D. students, supervised `8` M.Sc. students, supervised `13` B.Sc. students and hosted `6` visiting students from U.S., Sweden and India.
+# - Wrote and applied for research grants. Served as Principal Investigator for research grants that have been awarded `12.5 million THB` and `1.117 million SEK` in research funding from Thai and Swedish grant agencies.
+# - Conducted research by applying machine learning to computational drug discovery and ensuring that research output exceeds `20+` articles per year.
+# - Taught `10+` undergraduate/graduate classes on Bioinformatics, Data Mining, Scientific Research and Presentation, Research Methodology, Graduate Seminar, Programming for Health Data Science, etc.
+# - Peer reviewed `100+` research articles for leading scientific journals.
+# ''')
+
+# txt('**Co-Chair**, International Conference on Pharmaceutical Bioinformatics at Pattaya, Thailand',
+# '2016')
+# st.markdown('''
+# - Oversee all aspects of the conference preparations from conception to launch. This include inviting keynote and plenary speakers, create advertisement flyers, create abstract book, etc.
+# - Conference attracted `200+` participants from `40+` countries from university and industry sector.
+# - Chaired keynote session, technical workshop and some of the parallel sessions.
+# ''')
+
+# txt('**Content Creator**, [Data Professor YouTube Channel](https://youtube.com/dataprofessor/)',
+# '2019-Present')
+# st.markdown('''
+# - `100,000+` subscribers on YouTube
+# - Created `261` educational videos on data science, machine learning and bioinformatics as well as hosted several podcast episodes with data scientists.
+# - Created `3` sponsored videos for Notion, Gradio and Classpert.
+# ''')
+
+# txt('**Content Creator**, [Coding Professor YouTube Channel](https://youtube.com/codingprofessor/)',
+# '2019-Present')
+# st.markdown('''
+# - `3,200+` subscribers on YouTube
+# - Created `38` educational videos on Python and R programming.
+# ''')
+
+# txt('**Technical Writer**, [Data Professor Blog](https://data-professor.medium.com/) on Medium.com',
+# '2019-Present')
+# st.markdown('''
+# - `4,100+` subscribers on Medium
+# - Written `68` technical blogs on data science, machine learning and bioinformatics.
+# ''')
 
 #####################
 st.markdown('''
-## Work Experience
+## Code development
 ''')
-
-txt('**Head, Center of Data Mining and Biomedical Informatics**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2011-2021')
-st.markdown('''
-- Managing a Center of `10` professors, researchers and students to ensure KPIs are strategically achieved namely to publish at least `20+` research publications annually. 
-- Actively took part in the talent hiring process as well as help employees to plan and develop their career path.
-- Set budget and handle procurement in order to facilitate education and research activities. Secured `> 10 million THB` budget.
-- Set and reflect on OKR on an annual basis to ensure productivity strategically matches the organization's direction.
-''')
-
-txt('**Associate Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2012-2021')
-txt('**Assistant Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2009-2012')
-txt('**Lecturer**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2006-2009')
-st.markdown('''
-- Provided mentorship and supervision to junior faculty, researchers, Ph.D./M.Sc./B.Sc. students. Mentored `3` Post-doctoral fellows, supervised `13` Ph.D. students, supervised `8` M.Sc. students, supervised `13` B.Sc. students and hosted `6` visiting students from U.S., Sweden and India.
-- Wrote and applied for research grants. Served as Principal Investigator for research grants that have been awarded `12.5 million THB` and `1.117 million SEK` in research funding from Thai and Swedish grant agencies.
-- Conducted research by applying machine learning to computational drug discovery and ensuring that research output exceeds `20+` articles per year.
-- Taught `10+` undergraduate/graduate classes on Bioinformatics, Data Mining, Scientific Research and Presentation, Research Methodology, Graduate Seminar, Programming for Health Data Science, etc.
-- Peer reviewed `100+` research articles for leading scientific journals.
-''')
-
-txt('**Co-Chair**, International Conference on Pharmaceutical Bioinformatics at Pattaya, Thailand',
-'2016')
-st.markdown('''
-- Oversee all aspects of the conference preparations from conception to launch. This include inviting keynote and plenary speakers, create advertisement flyers, create abstract book, etc.
-- Conference attracted `200+` participants from `40+` countries from university and industry sector.
-- Chaired keynote session, technical workshop and some of the parallel sessions.
-''')
-
-txt('**Content Creator**, [Data Professor YouTube Channel](https://youtube.com/dataprofessor/)',
-'2019-Present')
-st.markdown('''
-- `100,000+` subscribers on YouTube
-- Created `261` educational videos on data science, machine learning and bioinformatics as well as hosted several podcast episodes with data scientists.
-- Created `3` sponsored videos for Notion, Gradio and Classpert.
-''')
-
-txt('**Content Creator**, [Coding Professor YouTube Channel](https://youtube.com/codingprofessor/)',
-'2019-Present')
-st.markdown('''
-- `3,200+` subscribers on YouTube
-- Created `38` educational videos on Python and R programming.
-''')
-
-txt('**Technical Writer**, [Data Professor Blog](https://data-professor.medium.com/) on Medium.com',
-'2019-Present')
-st.markdown('''
-- `4,100+` subscribers on Medium
-- Written `68` technical blogs on data science, machine learning and bioinformatics.
-''')
-
-#####################
-st.markdown('''
-## Bioinformatics Tools
-''')
-txt4('ABCpred', 'A web server for the discovery of acetyl- and butyryl-cholinesterase inhibitors', 'http://codes.bio/abcpred/')
-txt4('AutoWeka', 'An automated data mining software based on Weka', 'http://www.mt.mahidol.ac.th/autoweka/')
-txt4('ACPred', 'A computational tool for the prediction and analysis of anticancer peptides','http://codes.bio/acpred/')
-txt4('BioCurator', 'A web server for curating ChEMBL bioactivity data', 'http://codes.bio/biocurator/')
-txt4('CryoProtect', 'A web server for classifying antifreeze proteins from non-antifreeze proteins','http://codes.bio/cryoprotect/')
+txt4('TURBOMOLE', 'DFT based embedding coupled with WFT and RT-TDDFT methods within the RIPER module of the popular TURBOMOLE package', 'https://www.turbomole.org/')
+txt4('CrysX-NN', 'An efficient neural network library from scratch that supports parallelization and GPUs', 'https://github.com/manassharma07/crysx_nn')
+txt4('CrysX-3D Viewer', 'A molecule and crystal viewer that renders high quality visualizations using complex shaders developed using Unity gaming engine. Avaliable on Android, Windows, Mac and Linux','https://www.bragitoff.com/crysx-3d-viewer/')
+txt4('CrysX-AR', 'An android app for augmented reality visualization of molecules and crystals', 'https://play.google.com/store/apps/details?id=com.bragitoff.crysxar')
+txt4('CrysX-Crystallographic Tools', 'A web server for classifying antifreeze proteins from non-antifreeze proteins','https://play.google.com/store/apps/details?id=com.bragitoff.powderdiffractionsimulator')
 txt4('ERpred', 'A web server for the prediction of subtype-specific estrogen receptor antagonists', 'http://codes.bio/erpred')
 txt4('HCVpred', 'A web server for predicting the bioactivity of Hepatitis C virus NS5B inhibitors', 'http://codes.bio/hemopred/')
 txt4('HemoPred', 'A web server for predicting the hemolytic activity of peptides', 'http://codes.bio/hemopred/')
