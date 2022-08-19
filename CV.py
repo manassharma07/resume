8c21144edb661cadd17786e2dba50047d40f10dd
manas sharma <mikeatthetop@gmail.com>
2022-08-19 16:16:27 +0200

    changed navbar settings

--- CV.py
+++ CV.py
@@ -42,7 +42,7 @@ Besides all this, I love making [YouTube tutorials](https://www.youtube.com/c/Br
 # st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+<nav class="navbar fixed-top navbar-expand navbar-dark" style="background-color: #16A2CB;">
   <a class="navbar-brand" href="https://www.bragitoff.com/about/" target="_blank">Manas Sharma</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavNew" aria-controls="navbarNavNew" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
